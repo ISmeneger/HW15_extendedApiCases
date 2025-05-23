85aa2bfb4c7ee87c5ead887ea79ef5f80d34663a
ISmeneger <ISmeneger@yandex.ru>
2025-05-23 20:44:22 +0300

    Add pdf utils file

---
+++ src/test/java/utils/PdfUtils.java
@@ -0,0 +1,32 @@
+package utils;
+
+import io.restassured.response.Response;
+
+import java.io.*;
+
+public class PdfUtils {
+    public static void savePdf(Response response, String fileName) {
+        if (response != null) {
+            // Получение содержимого ответа в виде InputStream
+            try (InputStream inputStream = response.getBody().asInputStream()) {
+                // Создание файла для сохранения PDF
+                OutputStream outputStream = new FileOutputStream(fileName);
+
+                // Копирование содержимого InputStream в файл
+                int bytesRead;
+                byte[] buffer = new byte[4096];
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                // Закрытие потоков
+                inputStream.close();
+                outputStream.close();
+
+                System.out.println("PDF успешно сохранен в файл: " + fileName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
