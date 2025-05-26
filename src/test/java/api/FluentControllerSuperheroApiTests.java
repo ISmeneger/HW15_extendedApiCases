f312a826ea6ba8f0cd7f4dc44aa42c4f8d6b0dc2
ISmeneger <ISmeneger@yandex.ru>
2025-05-26 19:38:16 +0300

    Add api tests

---
+++ src/test/java/api/FluentControllerSuperheroApiTests.java
@@ -0,0 +1,73 @@
+package api;
+
+import controllers.FluentSuperheroController;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static testdata.SuperheroTestData.*;
+
+public class FluentControllerSuperheroApiTests {
+    FluentSuperheroController fluentSuperheroController = new FluentSuperheroController();
+
+    @BeforeEach
+    @AfterEach
+    void clear() {
+        fluentSuperheroController.deleteSuperheroById(CREATE_NEW_SUPERHERO.getId());
+    }
+
+    @Test
+    @DisplayName("Check create new superhero is returns 200 status ok")
+    void checkAddNewSuperheroTest() {
+        fluentSuperheroController.createSuperhero(CREATE_NEW_SUPERHERO).statusCodeIs(200);
+    }
+
+    @Test
+    @DisplayName("Create superhero without phone, check status code and response body")
+    void checkSuperheroWithoutPhoneTest() throws InterruptedException {
+//        String expectedId = fluentSuperheroController.createSuperhero(SUPERHERO_WITHOUT_PHONE)
+//                .statusCodeIs(200)
+//                .getJsonValue("id");
+
+        fluentSuperheroController.createSuperhero(SUPERHERO_WITHOUT_PHONE)
+                .statusCodeIs(200);
+
+        Thread.sleep(3000);
+        fluentSuperheroController.getSuperheroesById(SUPERHERO_WITHOUT_PHONE.getId())
+                .statusCodeIs(200)
+                .jsonValueIs("birthDate", SUPERHERO_WITHOUT_PHONE.getBirthDate())
+                .jsonValueIs("city", SUPERHERO_WITHOUT_PHONE.getCity())
+                .jsonValueIs("fullName", SUPERHERO_WITHOUT_PHONE.getFullName())
+                .jsonValueIs("gender", String.valueOf(SUPERHERO_WITHOUT_PHONE.getGender()))
+                .jsonValueIs("id", String.valueOf(SUPERHERO_WITHOUT_PHONE.getId()))
+                .jsonValueIs("mainSkill", SUPERHERO_WITHOUT_PHONE.getMainSkill())
+                .jsonValueIs("phone", null);
+    }
+
+    @Test
+    @DisplayName("Check get all superhero")
+    void checkGetAllSuperheroesTest() {
+        fluentSuperheroController.getAllSuperheroes().statusCodeIs(200);
+    }
+
+    @Test
+    @DisplayName("Check get superhero by id")
+    void checkGetSuperheroByIdTest() {
+        fluentSuperheroController.getSuperheroesById(10).statusCodeIs(200);
+    }
+
+    @Test
+    @DisplayName("Check update superhero by id")
+    void checkUpdateSuperheroByIdTest() {
+        fluentSuperheroController.updateSuperhero(UPDATE_SUPERHERO, 200).statusCodeIs(400);
+    }
+
+    @Test
+    @DisplayName("Check update superhero by id")
+    void checkDeleteSuperheroByIdTest() {
+        fluentSuperheroController.deleteSuperheroById(10).statusCodeIs(200);
+    }
+}
+
+
